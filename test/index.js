b53e5d9b09bcef3370727ba80d9c11fb2ac7f89a
Fabio Spampinato <spampinabio@gmail.com>
2023-08-25 02:45:42 +0100

    Added a test

--- test/index.js
+++ test/index.js
@@ -2,15 +2,27 @@
 /* IMPORT */
 
 import {describe} from 'fava';
-import packer from '../dist/index.js';
+import fs from 'node:fs';
+import {pack, unpack} from '../dist/index.js';
 
 /* MAIN */
 
-describe ( 'Packer', it => {
+describe ( 'Siar', it => {
 
-  it ( 'works', t => {
+  it ( 'can pack and unpack a folder', t => {
 
-    //TODO
+    pack ( 'node_modules', 'node_modules.siar' );
+    unpack ( 'node_modules.siar', 'node_modules_2' );
+    pack ( 'node_modules_2', 'node_modules_2.siar' );
+
+    const archive1 = fs.readFileSync ( 'node_modules.siar' );
+    const archive2 = fs.readFileSync ( 'node_modules_2.siar' );
+
+    fs.rmdirSync ( 'node_modules_2', { recursive: true } );
+    fs.unlinkSync ( 'node_modules.siar' );
+    fs.unlinkSync ( 'node_modules_2.siar' );
+
+    t.deepEqual ( archive1, archive2 );
 
   });
 
