86c92a375ab028fcd2bc8844af4d47f21815a8b9
Fabio Spampinato <spampinabio@gmail.com>
2023-08-25 03:33:20 +0100

    Added tests for read and get

--- test/index.js
+++ test/index.js
@@ -3,7 +3,8 @@
 
 import {describe} from 'fava';
 import fs from 'node:fs';
-import {pack, unpack} from '../dist/index.js';
+import U8 from 'uint8-encoding';
+import {get, pack, read, unpack} from '../dist/index.js';
 
 /* MAIN */
 
@@ -23,6 +24,33 @@ describe ( 'Siar', it => {
     fs.unlinkSync ( 'node_modules_2.siar' );
 
     t.deepEqual ( archive1, archive2 );
+
+  });
+
+  it ( 'can read a file from an archive', t => {
+
+    pack ( 'node_modules', 'node_modules.siar' );
+
+    const file = read ( 'node_modules.siar', 'fava/package.json' );
+    const content = fs.readFileSync ( 'node_modules/fava/package.json', 'utf8' );
+
+    fs.unlinkSync ( 'node_modules.siar' );
+
+    t.deepEqual ( U8.decode ( file.content ), content );
+
+  });
+
+  it ( 'can get a file from an archive', t => {
+
+    pack ( 'node_modules', 'node_modules.siar' );
+
+    const archive = fs.readFileSync ( 'node_modules.siar' );
+    const file = get ( archive, 'fava/package.json' );
+    const content = fs.readFileSync ( 'node_modules/fava/package.json', 'utf8' );
+
+    fs.unlinkSync ( 'node_modules.siar' );
+
+    t.deepEqual ( U8.decode ( file.content ), content );
 
   });
 
